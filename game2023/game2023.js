5ce12458f386a5edb9a09f736310a709e255673f
Joshua Chan <soulhexcalibur101@gmail.com>
2023-11-11 22:37:55 -0800

    updating code on the portfolio page

--- game2023/game2023.js
+++ game2023/game2023.js
@@ -4,7 +4,16 @@ var ballSize = 40;
 var score = 0;
 var gameState = "L1";
 let timer = 5
+var img, img2, img3; 
 
+function preload() {
+// preload() runs once, it may make you wait
+//  img = loadImage('cat.jpg')  // cat.jpg needs to be next to this .js file
+// you can link to an image on your github account
+  img = loadImage('https://drainblazor46.github.io/carltondance.gif');
+  img2 = loadImage('https://drainblazor46.github.io/nyancat.gif');
+  img3 = loadImage('https://drainblazor46.github.io/rockeyebrow.gif');  
+}
 function setup() {
 createCanvas(600, 600);
   textAlign(CENTER);
@@ -24,6 +33,10 @@ background(200);
  if(gameState == "L3"){
    levelThree();
  }
+ if (gameState=="win"){
+     text("You Won", width/2, height-20);
+  }
+
 text(("Score: " + score),width/2,40);
 text(("Timer: " + timer), width/2, 20);
 } // end of draw
@@ -47,8 +60,11 @@ function levelOne(){
     gameState = "L2";
   }
   line(ballx, bally, mouseX, mouseY);
-  ellipse(ballx, bally, ballSize);
+  // ellipse(ballx, bally, ballSize);
   
+    image(img, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
+    
+    
 } // end of level 1 ===========================================================================
 
 function levelTwo(){
@@ -71,7 +87,9 @@ function levelTwo(){
       gameState = "L3";
     }
   line(ballx, bally, mouseX, mouseY);
-  ellipse(ballx, bally, ballSize);
+  // ellipse(ballx, bally, ballSize);
+  
+  image(img2, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
   
 } // end of level 2 ===========================================================================
 
@@ -83,7 +101,7 @@ function levelThree(){
     ballx = random(width);
     bally = random(height);
     score = score + 1;
-    ballSize = ballSize - 5;
+    ballSize = ballSize - 2;
   }
   if(frameCount % 60 == 0 && timer > 0){
     timer --;
@@ -92,10 +110,15 @@ function levelThree(){
     text("GAME OVER", width/2, height*0.7);
   }
   if(score>= 15){
-   // timer = timer + 5;
-      background(random(255));
+    // timer = timer + 5;
+      background(120);
     }
   line(ballx, bally, mouseX, mouseY);
-  ellipse(ballx, bally, ballSize);
+   ellipse(ballx, bally, ballSize);
   
-} // end of level 3 ===========================================================================
+    image(img3, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
+} 
+if(score>25){
+// level 4
+  gameState = "win";
+}
